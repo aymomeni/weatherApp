dc2812829e0a0ca4a645e767e4ba61a066d65e4f
Ali Momeni <aymomeni@gmail.com>
2022-02-06 10:36:35 -0700

    added axios

--- weather-app/src/main.js
+++ weather-app/src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import * as VueGoogleMaps from 'vue2-google-maps'
+import axios from 'axios'
 
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.use(VueGoogleMaps, {
